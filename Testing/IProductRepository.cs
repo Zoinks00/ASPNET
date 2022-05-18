ab7b42f181bcd4c57d2de01f01cb78507eb2632a
Forrest Rabideau <forrest.rabideau@gmail.com>
2022-05-18 02:20:27 -0700

    Added Insert,Delete,Update

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -12,5 +12,10 @@ namespace Testing
 
         public Product GetProduct(int id);
         public void UpdateProduct(Product product);
+        public void InsertProduct(Product productToInsert);
+        public void DeleteProduct(Product product);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+
     }
 }
--- Testing/ProductRepository.cs
+++ Testing/ProductRepository.cs
@@ -8,10 +8,10 @@ using Dapper;
 
 namespace Testing
 {
-    public class ProductRepository: IProductRepository
+    public class ProductRepository : IProductRepository
     {
         private readonly IDbConnection _conn;
-        
+
         //constructor
         public ProductRepository(IDbConnection conn)
         {
@@ -20,7 +20,7 @@ namespace Testing
 
         public IEnumerable<Product> GetAllProducts()
         {
-            return _conn.Query<Product>("SELECT * FROM PRODUCTS"); 
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS");
         }
 
         //method to get products
@@ -29,12 +29,44 @@ namespace Testing
             return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id",
                new { id = id });
         }
-//method to update products
+        //method to update products
         public void UpdateProduct(Product product)
         {
             _conn.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id",
                 new { name = product.Name, price = product.Price, id = product.ProductID });
         }
+
+        public void InsertProduct(Product productToInsert)
+        {
+            _conn.Execute("INSERT INTO products (NAME, PRICE, CATEGORYID) VALUES (@name, @price, @categoryID);",
+                new { name = productToInsert.Name, price = productToInsert.Price, categoryID = productToInsert.CategoryID });
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return _conn.Query<Category>("SELECT * FROM categories;");
+        }
+
+        public Product AssignCategory()
+        {
+            var categoryList = GetCategories();
+            var product = new Product();
+            product.Categories = categoryList;
+
+            return product;
+        }
+
+        //delete a product from the database
+        public void DeleteProduct(Product product)
+        {
+            _conn.Execute("DELETE FROM REVIEWS WHERE ProductID = @id;",
+                                       new { id = product.ProductID });
+            _conn.Execute("DELETE FROM Sales WHERE ProductID = @id;",
+                                       new { id = product.ProductID });
+            _conn.Execute("DELETE FROM Products WHERE ProductID = @id;",
+                                       new { id = product.ProductID });
+        }
+
 
     }
 }
